6767e454d79664d878928ed5aeab281fecff0fcd
seantokuzo <seantokuzo@gmail.com>
2023-03-01 19:38:53 -0800

    add kuzo colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        kuzoPurp: '#b077ba',
+        kuzoOrange: '#ffad61'
+      },
       fontSize: {
         xxxs: '0.45rem',
         xxs: '0.6rem'
